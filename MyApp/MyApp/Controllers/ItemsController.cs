00a71b0aeed4ef5ed3f2a70600ef667426d79307
nikhilpj <nikhilpj98@gmail.com>
2024-10-15 20:11:56 +0530

    initial commit

---
+++ MyApp/MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly MyAppContext _context;
+        public ItemsController (MyAppContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var item =await _context.Items.ToListAsync();
+            return View(item);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price")] Item item)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+    }
+}
